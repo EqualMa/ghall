d42920cf885381b66ac7c8eac610a195682dde76
EqualMa <equalma@outlook.com>
2020-09-12 21:01:55 +0800

    feat: user resolver: user info

--- src/github/crawler/user.ts
+++ src/github/crawler/user.ts
@@ -3,7 +3,12 @@ import { ghUserUrl, ghUserResUrl } from "../url";
 import { memoizeWithThis } from "../../memo";
 import { encodeNodeId } from "../id";
 import { extractUserPinnedItemsFromCheerio } from "../../extract-pinned-item";
-import { PinnableItemConnection } from "../../graphql/generated";
+import {
+  PinnableItemConnection,
+  ResolversTypes,
+} from "../../graphql/generated";
+import got from "got/dist/source";
+import { ApolloError } from "apollo-server-micro";
 
 const getAvatarUrlObj = memoizeWithThis(function (this: UserExtractor) {
   const $ = this.$;
@@ -12,7 +17,7 @@ const getAvatarUrlObj = memoizeWithThis(function (this: UserExtractor) {
   return new URL(a);
 });
 
-const REGEX_DATABASE_ID_FROM_URL = /^\/u\/\d+$$/;
+const REGEX_DATABASE_ID_FROM_URL = /^\/u\/(\d+)$/;
 const getDatabaseId = memoizeWithThis(function (this: UserExtractor) {
   const res = REGEX_DATABASE_ID_FROM_URL.exec(this._avatarUrlObj.pathname);
   if (!res)
@@ -57,12 +62,15 @@ const getLocation = memoizeWithThis(function (this: UserExtractor) {
   return $el.length ? $el.text() : null;
 });
 
-const getEmail = memoizeWithThis(function (this: UserExtractor) {
-  return this.$("[itemprop=email]").text();
+const getEmail = memoizeWithThis(function (this: UserExtractor): string {
+  const mailTo = this.$("[itemprop=email] > a").attr("href");
+  return mailTo?.startsWith("mailto:") ? mailTo.slice("mailto:".length) : "";
 });
 
-const getWebsiteUrl = memoizeWithThis(function (this: UserExtractor) {
-  return this.$("[itemprop=url]").text();
+const getWebsiteUrl = memoizeWithThis(function (
+  this: UserExtractor,
+): string | null {
+  return this.$("[itemprop=url] > a").attr("href") ?? null;
 });
 
 const getPinnedItems = memoizeWithThis(function (
@@ -87,6 +95,39 @@ const getBio = memoizeWithThis(function (this: UserExtractor) {
 
 const getBioHTML = memoizeWithThis(function (this: UserExtractor) {
   return this.$bio.html() ?? "";
+});
+
+const status = memoizeWithThis(function (
+  this: UserExtractor,
+): ResolversTypes["UserStatus"] | null {
+  const $el = this.$(".user-status-circle-badge-container");
+
+  if (!$el.length) return null;
+
+  const $gEmoji = $el.find("g-emoji");
+
+  const alias = $gEmoji.attr("alias");
+  const emoji: string | null = alias ? `:${alias}:` : null;
+  const emojiHTML: string | null = this.$.html($gEmoji.parent());
+
+  const message =
+    $el.find(".user-status-message-wrapper").text().trim() || null;
+
+  return {
+    emoji,
+    emojiHTML,
+    message,
+    user: this,
+  };
+});
+
+const TWITTER_BASE_URL = "https://twitter.com/";
+const twitterUsername = memoizeWithThis(function (this: UserExtractor) {
+  const href = this.$("[itemprop=twitter] > a").attr("href");
+
+  return href && href.startsWith(TWITTER_BASE_URL)
+    ? href.slice(TWITTER_BASE_URL.length)
+    : null;
 });
 
 export class UserExtractor extends CheerioExtractor {
@@ -128,6 +169,14 @@ export class UserExtractor extends CheerioExtractor {
     return getBioHTML.call(this);
   }
 
+  get status() {
+    return status.call(this);
+  }
+
+  get twitterUsername() {
+    return twitterUsername.call(this);
+  }
+
   get $company(): Cheerio {
     return $company.call(this);
   }
@@ -163,8 +212,25 @@ export class UserExtractor extends CheerioExtractor {
   }
 }
 
+export class UserNotFoundApolloError extends ApolloError {
+  constructor(public readonly login: string) {
+    super(
+      `Could not resolve to a User with the login of '${login}'.`,
+      "USER_NOT_FOUND",
+      { type: "NOT_FOUND" },
+    );
+  }
+}
+
 export class UserCrawler extends WebPageCrawler<UserExtractor> {
-  constructor(login: string) {
+  constructor(public readonly login: string) {
     super(ghUserUrl(login), ($) => new UserExtractor($, login));
+  }
+
+  async handleError(err: unknown) {
+    if (err instanceof got.HTTPError && err.response.statusCode === 404)
+      throw new UserNotFoundApolloError(this.login);
+
+    return super.handleError(err);
   }
 }
--- src/graphql/resolver.ts
+++ src/graphql/resolver.ts
@@ -1,29 +1,17 @@
 import { Resolvers } from "./generated";
 import { decodeNodeId } from "../github/id";
-import { queryUserPinnedItemsFromHomepage } from "../pinned-items";
-import { UserExtractor, UserCrawler } from "../github/crawler/user";
+import { UserCrawler } from "../github/crawler/user";
 
 const resolvers: Resolvers = {
   Query: {
     user: async (parent, args) => {
       const { login } = args;
       const e = new UserCrawler(login);
-      return e;
+      return e.getResource();
     },
   },
   User: {
-    url: async (parent) => {
-      const e = await parent.getResource();
-      return e.url;
-    },
-    websiteUrl: async (parent) => {
-      const e = await parent.getResource();
-      return e.websiteUrl;
-    },
-    avatarUrl: async (parent, args) => {
-      const e = await parent.getResource();
-      return e.getAvatarUrl(args.size);
-    },
+    avatarUrl: (e, args) => e.getAvatarUrl(args.size),
   },
   Node: {
     __resolveType: (parent) => {
